91f9e806ecbdbdbcba5843f3ab0c1b12e284268f
Equan Pr <equan.p@gmail.com>
2020-04-19 05:27:53 +0700

    Basic Enum

---
+++ src/enum.ts
@@ -0,0 +1,23 @@
+/**
+ * Enum di TypeScript
+ * ------------------
+ * 
+ * Seperti halnya Boolean (true, false) di JavaScript, di TypeScript value yang sama bisa di
+ * buat dengan bantuan Enum.
+ * 
+ * Default value dari Enum member adalah numerik!
+ * 
+ * assert.equal(YesNo.Yes, 0) // true
+ * assert.equa.(YesNo,1 ) // true
+ */
+export enum YesNo {
+  Yes, // member 1
+  No, // member2, trailing comma allows and ignored
+}
+
+// Enum dengan Inisialisasi
+export enum NoYes {
+  No = 1,
+  Yes = 2,
+}
+
